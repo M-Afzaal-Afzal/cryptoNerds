2dd4f12528a8e9d832cc6e204efe91b086f090fb
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-21 22:34:01 +0500

    icons updated

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -18,29 +18,29 @@ const Footer = () => {
 
                 <Fade bottom cascade>
 
-                <Typography sx={{
-                    textAlign: 'center',
-                    fontSize: '60px',
-                    fontWeight: 'bold',
-                    mx: 'auto',
-                    color: '#fff',
-                    py: "6rem",
-                }} variant={'h1'}>
-                    CRYPTO NERDS
-                </Typography>
+                    <Typography sx={{
+                        textAlign: 'center',
+                        fontSize: '60px',
+                        fontWeight: 'bold',
+                        mx: 'auto',
+                        color: '#fff',
+                        py: "6rem",
+                    }} variant={'h1'}>
+                        CRYPTO NERDS
+                    </Typography>
 
-                {/* Bottom Section*/}
-                <Box sx={{
-                    display: 'grid',
-                    gridTemplateColumns: {xs: '1fr',lg:'auto'},
-                    justifyContent: 'center',
-                    gridGap: '2rem',
-                    alignItems: 'center',
-                    pb: '4rem',
-                }}>
+                    {/* Bottom Section*/}
+                    <Box sx={{
+                        display: 'grid',
+                        gridTemplateColumns: {xs: '1fr', lg: 'auto'},
+                        justifyContent: 'center',
+                        gridGap: '2rem',
+                        alignItems: 'center',
+                        pb: '4rem',
+                    }}>
 
-                    {/* Left Items*/}
-                    {/*<Box>*/}
+                        {/* Left Items*/}
+                        {/*<Box>*/}
                         {/*<Typography variant={'h5'} sx={{*/}
                         {/*    color: '#fff',*/}
                         {/*    ml: '1rem',*/}
@@ -61,56 +61,66 @@ const Footer = () => {
                         {/*}}>*/}
                         {/*    email@email.com*/}
                         {/*</Box>*/}
-                    {/*</Box>*/}
+                        {/*</Box>*/}
 
-                    {/* Right Items*/}
-                    <Box>
-                        <Typography variant={'h5'} sx={{
-                            color: '#fff',
-                            ml: '1rem',
-                            textAlign: 'center',
-                        }}>
-                            Join The Community
-                        </Typography>
-
-                        <Box sx={{
-                            mt: '1rem',
-                        }}>
-                            {/* Left side icons*/}
-                            <Stack sx={{
+                        {/* Right Items*/}
+                        <Box>
+                            <Typography variant={'h5'} sx={{
                                 color: '#fff',
-                                justifyContent: 'center',
-                            }} spacing={1} alignItems={'center'} direction={'row'}>
-                                {/*<IconButton sx={{*/}
-                                {/*    display:'flex',*/}
-                                {/*}} color={'secondary'}>*/}
-                                {/*    <TelegramIcon fontSize={'30px'}/>*/}
-                                {/*</IconButton>*/}
+                                ml: '1rem',
+                                textAlign: 'center',
+                            }}>
+                                Join The Community
+                            </Typography>
 
-                                <IconButton sx={{
-                                    display:'flex',
+                            <Box sx={{
+                                mt: '1rem',
+                            }}>
+                                {/* Left side icons*/}
+                                <Stack sx={{
+                                    color: '#fff',
+                                    justifyContent: 'center',
+                                }} spacing={1} alignItems={'center'} direction={'row'}>
+                                    {/*<IconButton sx={{*/}
+                                    {/*    display:'flex',*/}
+                                    {/*}} color={'secondary'}>*/}
+                                    {/*    <TelegramIcon fontSize={'30px'}/>*/}
+                                    {/*</IconButton>*/}
 
-                                }} color={'secondary'}>
-                                    <InstagramIcon fontSize={'30px'}/>
-                                </IconButton>
+                                    <IconButton component={'a'}
+                                                href={'https://www.instagram.com/cryptonerds_nft/'}
+                                                target={'_blank'} sx={{
+                                        display: 'flex',
 
-                                <IconButton sx={{
-                                    display:'flex',
+                                    }} color={'secondary'}>
+                                        <InstagramIcon fontSize={'30px'}/>
+                                    </IconButton>
 
-                                }} color={'secondary'}>
-                                    <TwitterIcon fontSize={'30px'}/>
-                                </IconButton>
+                                    <IconButton component={'a'}
+                                                href={'https://twitter.com/CryptoNerdsNFT1?s=20'}
+                                                target={'_blank'}
+                                                rel={'noreferrer'}
+                                                sx={{
+                                                    display: 'flex',
 
-                                <IconButton>
-                                    <Image width={25} height={25} src={'/openSea.svg'}/>
-                                </IconButton>
-                            </Stack>
+                                                }} color={'secondary'}>
+                                        <TwitterIcon fontSize={'30px'}/>
+                                    </IconButton>
+
+                                    <IconButton component={'a'}
+                                                href={'https://opensea.io/'}
+                                                target={'_blank'}
+                                                rel={'noreferrer'}
+                                    >
+                                        <Image width={25} height={25} src={'/openSea.svg'}/>
+                                    </IconButton>
+                                </Stack>
+
+                            </Box>
 
                         </Box>
 
                     </Box>
-
-                </Box>
                 </Fade>
 
             </Container>
